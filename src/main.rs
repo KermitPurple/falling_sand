a6532e4243296937724abb14eafb20ea0d199775
Shane McDonough <shanemcdman@gmail.com>
2021-08-17 19:01:21 -0700

    Finish falling sand sim

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,19 @@
 mod coord;
 use ggez::{
+    timer,
     Context,
     ContextBuilder,
     GameResult,
     GameError,
     conf::{WindowSetup, WindowMode},
     event::{self, EventHandler},
-    input::keyboard::{
-        self,
-        KeyCode,
-        KeyMods,
+    input::{
+        mouse::MouseButton,
+        keyboard::{
+            self,
+            KeyCode,
+            KeyMods,
+        },
     },
     graphics::{
         self,
@@ -19,6 +23,7 @@ use ggez::{
         DrawParam,
     },
 };
+use rand::prelude::*;
 
 type Point = coord::Coord;
 
@@ -51,6 +56,14 @@ struct Game {
 
 impl Game {
     fn new(_ctx: &mut Context, conf: GameConf, style: Style) -> Self {
+        Self {
+            conf,
+            style,
+            grid: Game::_new_grid(&conf),
+        }
+    }
+
+    fn _new_grid(conf: &GameConf) -> Vec<Vec<Cell>> {
         let w = conf.board_size.x as usize;
         let h = conf.board_size.y as usize;
         let mut grid = Vec::with_capacity(h);
@@ -61,22 +74,22 @@ impl Game {
             }
             grid.push(v);
         }
-        Self {
-            conf,
-            style,
-            grid,
-        }
+        grid
+    }
+
+    fn new_grid(&self) -> Vec<Vec<Cell>> {
+        Game::_new_grid(&self.conf)
     }
 
     fn draw_cells(&mut self, ctx: &mut Context) -> GameResult {
         let w = self.conf.board_size.x as usize;
         let h = self.conf.board_size.y as usize;
-        let mut mb = graphics::MeshBuilder::new();
         for i in 0..h {
             for j in 0..w {
                 let cell = self.grid[i][j];
                 if cell.visible() {
-                    mb.rectangle(
+                    let rect = graphics::Mesh::new_rectangle(
+                        ctx,
                         DrawMode::fill(),
                         Rect::new(
                             j as f32 * self.conf.cell_size,
@@ -86,11 +99,11 @@ impl Game {
                         ),
                         cell.color(),
                     )?;
+                    graphics::draw(ctx, &rect, DrawParam::default())?;
                 }
             }
         }
-        let mesh = mb.build(ctx)?;
-        graphics::draw(ctx, &mesh, DrawParam::default())
+        Ok(())
     }
 
     fn draw_grid(&mut self, ctx: &mut Context) -> GameResult {
@@ -120,17 +133,78 @@ impl Game {
         let mesh = mb.build(ctx)?;
         graphics::draw(ctx, &mesh, DrawParam::default())
     }
+
+    fn in_grid(&self, x: usize, y: usize) -> bool {
+        let w = self.conf.board_size.x as usize;
+        let h = self.conf.board_size.y as usize;
+        x < w && y < h
+    }
+
+    fn is_empty(&self, x: usize, y: usize) -> bool {
+        self.grid[y][x] == Cell::Air
+    }
+
+    fn check_cell(&self, x: usize, y: usize) -> bool {
+        self.in_grid(x, y) && self.is_empty(x, y) 
+    }
+
+    fn move_cell(&mut self, x: usize, y: usize, to_x: usize, to_y: usize){
+        assert!(self.check_cell(to_x, to_y));
+        let cell = self.grid[y][x];
+        self.grid[y][x] = Cell::Air;
+        self.grid[to_y][to_x] = cell;
+    }
+    
+    fn set_cell(&mut self, x: usize, y: usize, cell: Cell) {
+        if self.in_grid(x, y) {
+            self.grid[y][x] = cell;
+        }
+    }
 }
 
 impl EventHandler<GameError> for Game {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        if !timer::check_update_time(ctx, self.conf.fps) {
+            return Ok(());
+        }
+        let w = self.conf.board_size.x as usize;
+        let h = self.conf.board_size.y as usize;
+        for i in (0..h).rev() {
+            for j in 0..w {
+                let cell = self.grid[i][j];
+                match cell {
+                    Cell::Air => (),
+                    Cell::Sand => {
+                        if self.check_cell(j, i + 1) { // cell below
+                            self.move_cell(j, i, j, i + 1);
+                        } else {
+                            match (
+                                self.check_cell(j + 1, i + 1),
+                                self.check_cell(j.wrapping_sub(1), i + 1),
+                            ) {
+                                (true, true) => if rand::random() {
+                                    self.move_cell(j, i, j + 1, i + 1)
+                                } else {
+                                    self.move_cell(j, i, j.wrapping_sub(1), i + 1)
+                                },
+                                (true, false) => self.move_cell(j, i, j + 1, i + 1),
+                                (false, true) => self.move_cell(j, i, j.wrapping_sub(1), i + 1),
+                                (false, false) => (),
+                            }
+                        }
+                    }
+                }
+            }
+        }
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, Color::BLACK);
         self.draw_cells(ctx)?;
-        self.draw_grid(ctx)?;
+        if self.conf.show_grid_lines {
+            self.draw_grid(ctx)?;
+        }
         graphics::present(ctx)
     }
 
@@ -147,6 +221,28 @@ impl EventHandler<GameError> for Game {
             _ => (),
         }
     }
+
+    fn mouse_button_down_event(
+        &mut self,
+        ctx: &mut Context,
+        button: MouseButton,
+        x: f32,
+        y: f32
+    ) {
+        match button {
+            MouseButton::Left => {
+                let x = (x / self.conf.cell_size) as usize;
+                let y = (y / self.conf.cell_size) as usize;
+                let s = self.conf.brush_size;
+                for i in *y.checked_sub(s).get_or_insert(0)..=(y + s) {
+                    for j in *x.checked_sub(s).get_or_insert(0)..=(x + s) {
+                        self.set_cell(j, i, Cell::Sand);
+                    }
+                }
+            },
+            _ => (),
+        }
+    }
 }
 
 #[derive(Copy, Clone, Debug)]
@@ -155,6 +251,8 @@ struct GameConf {
     board_size: Point,
     cell_size: f32,
     show_grid_lines: bool,
+    fps: u32,
+    brush_size: usize,
 }
 
 impl GameConf {
@@ -173,9 +271,11 @@ impl Default for GameConf {
     fn default() -> Self {
         Self {
             window_size: Point::new(2400., 1800.),
-            board_size: Point::new(480., 360.),
-            cell_size: 5.,
+            board_size: Point::new(120., 90.),
+            cell_size: 20.,
             show_grid_lines: false,
+            fps: 10,
+            brush_size: 10,
         }
     }
 }
@@ -195,8 +295,7 @@ impl Default for Style {
 }
 
 fn main() -> GameResult {
-    let conf = GameConf::new(Point::new(2400., 1800.), 60.);
-    // let conf = GameConf::default();
+    let conf = GameConf::default();
     let (mut ctx, event_loop) = ContextBuilder::new("Falling Sand", "KermitPurple")
         .window_setup(WindowSetup{
             title: String::from("Falling Sand"),
